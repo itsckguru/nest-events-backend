17606dc6131278430eff45370a9efc3cd08b780e
itsckguru <chandan@propertyguru.tech>
2023-12-05 15:37:11 +0530

    simple curd operation using local varialble

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -1,41 +1,59 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  Param,
-  Patch,
-  Post,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    Param,
+    Patch,
+    Post,
 } from '@nestjs/common';
 import { CreateEventDto } from '../dto/create-event.dto';
 import { UpdateEventDto } from 'src/dto/update-event.dto';
+import { Event } from './events.entity';
 
 @Controller('/events')
 export class EventsController {
-  @Get()
-  findAll(): string {
-    return 'This action returns all events';
-  }
+    private events: Event[] = [];
 
-  @Get(':id')
-  findOne(@Param('id') id): string {
-    return `This action returns a #${id} event, ${id}`;
-  }
+    @Get()
+    findAll() {
+        return this.events;
+    }
 
-  @Post()
-  create(@Body() input: CreateEventDto): object {
-    console.log(input.address);
-    return input;
-  }
+    @Get(':id')
+    findOne(@Param('id') id) {
+        const event = this.events.find((event) => event.id === parseInt(id));
+        return event;
+    }
 
-  @Patch(':id')
-  update(@Param('id') id, @Body() input: UpdateEventDto) {
-    return input;
-  }
-  @Delete(':id')
-  @HttpCode(204)
-  delete(): string {
-    return 'This action removes a #${id} event';
-  }
+    @Post()
+    create(@Body() input: CreateEventDto): object {
+        const event = {
+            ...input,
+            when: new Date(input.when),
+            id: this.events.length + 1,
+        };
+        this.events.push(event);
+        return event;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id, @Body() input: UpdateEventDto) {
+        const index = this.events.findIndex(
+            (event) => event.id === parseInt(id),
+        );
+
+        this.events[index] = {
+            ...this.events[index],
+            ...input,
+            when: input.when ? new Date(input.when) : this.events[index].when,
+        };
+        return this.events[index];
+    }
+    @Delete(':id')
+    @HttpCode(204)
+    remove(@Param('id') id) {
+        this.events = this.events.filter((event) => event.id !== parseInt(id));
+    }
 }
