dbc0b3f25d922a533b53c07307318f0834ffba44
itsckguru <chandan@propertyguru.tech>
2023-12-05 18:21:25 +0530

    fix: mysql connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events/events.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
-  controllers: [AppController, EventsController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: '127.0.0.1',
+            port: 3306,
+            username: 'root',
+            password: 'example',
+            database: 'nest-events',
+        }),
+    ],
+
+    controllers: [AppController, EventsController],
+    providers: [AppService],
 })
 export class AppModule {}
