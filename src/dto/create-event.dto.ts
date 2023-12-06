e7152aa49191d78daab63e03d26af526a9510a47
itsckguru <chandan@propertyguru.tech>
2023-12-06 09:26:55 +0530

    globle validators

--- src/dto/create-event.dto.ts
+++ src/dto/create-event.dto.ts
@@ -1,9 +1,12 @@
 import { Length } from 'class-validator';
 
 export class CreateEventDto {
-    @Length(5, 255)
+    @Length(5, 255, { message: 'Name must be at least 5 characters long' })
     name: string;
+    @Length(5, 255)
     description: string;
+    @Length(5, 255)
     when: Date;
+    @Length(5, 255)
     address: string;
 }
